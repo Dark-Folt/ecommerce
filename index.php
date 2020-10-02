64eec05cc72b8a8933c6d497455711bb418ff427
Dark-Folt <marigo9794@gmail.com>
2020-10-03 00:13:17 +0200

    debut page acceuil css

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE  html>
+<html>
+    <head>
+        <title>Accueil</title>
+       <meta charset="utf-8"> 
+       <link rel="stylesheet" type="text/css" href="/assets/css/accueil.css">
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <div id="div-a">
+                <a><img id ="logo" src="/assets/img/logo.png" alt="logo"></a>
+                <div id="div-categories">
+                    <p>Catégories</p>
+                </div>
+            </div>
+            <div id="div-b"><input type="search"></div>
+            <div id="div-c">
+                <!-- ICON DU Login -->
+                <a href="/connexion/index.php">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    </svg>
+                    <span>Connexion</span>
+                </a>
+                <a href="">
+                    <!-- ICON DU PANIER -->
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                    </svg>
+                    <span>Panier</span>
+                </a>
+            </div>
+        </header>  
+        <footer></footer>
+    </body>
+</html>
