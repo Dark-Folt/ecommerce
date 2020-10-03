80ffdd34901d3bacf5e65abc268b4db115c1d279
Dark-Folt <marigo9794@gmail.com>
2020-10-03 14:54:02 +0200

    ajout de la page connexion

---
+++ connexion/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Connexion</title>
+        <?php include "../include/head.html";?>
+    </head>
+    <body>
+        <?php include "../include/header.html"; ?> 
+    </body>
+</html>
