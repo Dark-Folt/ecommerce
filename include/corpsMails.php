c485a64b2755eecb7593a9ebe4c01e7d43c8bccf
Dark-Folt <marigo9794@gmail.com>
2020-10-15 00:46:20 +0200

    Supression du code css qui ne servé a rien et ajout de la fonction ecrireConfirmeMail

--- include/corpsMails.php
+++ include/corpsMails.php
@@ -1,42 +1,21 @@
 <?php
 //ce fichier contient que les corps des mails que l'on envoie
-$ConfirmCompte ='
-<html>
-        <head>
-            <title>Confirmation de votre mail</title>
-            <meta charset="utf-8">
-        </head>
-        <style type="text/css">
-            body{
-                margin: 0px;
-            }
-            div#message-container{
-                width: 500px;
-                margin: 100px auto 0px auto ;
-                border-radius: 20px;
-                background-color:  #f2f2f2;
-                box-shadow: 0px 5px 10px  #dedede;
-                padding: 25px;
-            }
-
-            a{
-                text-decoration: none;
-            }
-
-            a:hover {
-                color: #0066ff;
-            }
-        </style>
-        <body>
-            <div align="center" id="message-container">
-                <p>Votre compte a bien été crée, </br> 
-                Vérifier le en cliquant sur lien ci-dessous.
-                </p>
-                <a href="http://localhost/confirmation.php?pseudo=">Confirmez votre compte</a>
-            </div>
-        </body>
-    </html>
-';
+    function ecrireConfirmMessage($client_email, $confirm_token)
+    {
+        $s ="
+        <html>
+            <body>
+                <div align=\"center\" id=\"message-container\">
+                    <p>Votre compte a bien été crée, </br> 
+                    Vérifier le en cliquant sur lien ci-dessous.
+                    </p>
+                    <a href=\"http://localhost/ecommerce/controllers/mail_verification.php?email=$client_email&confirm_token=$confirm_token\">Confirmez votre compte</a>
+                </div>
+            </body>
+        </html>
+        ";
+        return $s;
+    }
 
 
 
