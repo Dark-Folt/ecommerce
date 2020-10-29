7e39430bd44a18beb32d2773c9c0d29d45977a83
Dark-Folt <marigo9794@gmail.com>
2020-10-29 01:53:27 +0100

    refactorisation des liens plus ajout de la page panier mais pas encore de gestion pour les sessions

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <html>
     <header class="container-fluid">
         <div class="row">
-            <div id="div-a" class="col-lg-3""> <!--Bootstrap separe la page en colonne-->
+            <div id="div-a" class="col-lg-3"> <!--Bootstrap separe la page en colonne-->
                 <a><img id ="logo" src="assets/img/logo.png" alt="logo"></a>
                 <div id="div-categories">
                     <ul class="nav navbar-nav">
@@ -49,7 +49,7 @@
                     </svg>
                     <span>Mon compte</span>
                 </a>
-                <a href="#" id="panier_lien" class="bg-success">
+                <a href="sessions/panier/panier.php" id="panier_lien" class="bg-success">
                     <!-- ICON DU PANIER -->
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
--- sessions/categories/documents.php
+++ sessions/categories/documents.php
@@ -1,5 +1,3 @@
 <?php
-session_start();
-echo "je video";
-session_destroy();
+echo "je suis dans document";
 ?>
