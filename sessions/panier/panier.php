e5ce2037925c7b2718c86c4a6f3743aea8fa0bc5
Dark-Folt <marigo9794@gmail.com>
2020-10-30 16:21:57 +0100

    creation du fichier panier

--- sessions/panier/panier.php
+++ sessions/panier/panier.php
@@ -1,1 +1,12 @@
-<?php echo 'je suis dans le panier' ?>
+<?php
+session_start();
+
+/*
+ *
+ * On verifie si le client est deja connecter
+*/
+
+if(!isset($_SESSION['connect'])){
+    header('Location: ../../include/errors/pageDErreur.php?panier=vide');
+}
+?>
