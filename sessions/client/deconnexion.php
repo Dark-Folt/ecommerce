b32419f07f1d5e94a0161a201d98dc580ee28315
Dark-Folt <marigo9794@gmail.com>
2020-10-31 23:44:20 +0100

    ajout de commentaires

--- sessions/client/deconnexion.php
+++ sessions/client/deconnexion.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+/**
+ * on verifi si la variable connect est defeni
+ * si elle est defini on fait un session_destroy()
+ * et on redirige le client vers la page de connexion
+ */
 if(isset($_SESSION['connect']))
 {
 	session_destroy();
