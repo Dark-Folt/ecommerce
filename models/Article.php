c09416fea86b46c6474469a9fa9c342f48231348
Dark-Folt <marigo9794@gmail.com>
2020-10-31 23:37:13 +0100

    ajout du model articles

--- models/Article.php
+++ models/Article.php
@@ -7,21 +7,42 @@ class Article
 	private $id,
 			$nom,
 			$marque,
+			$image,
+			$description,
 			$prix;
 	
-	public function __construct($article)
+	private function hydrate(array $donnees)
 	{
+		foreach($donnees as $key => $value)
+		{
+			$method = 'set'.ucfirst($key);
+			if(method_exists($this,$method))
+			{
+				$this->$method($value);
+			}
+		}
+	}
 
+	public function __construct(array $donnees)
+	{
+		if(!empty($donnees))
+		{
+			$this->hydrate($donnees);
+		}
 	}
 
 	public function getId() { return $this->id; }
 	public function getNom() { return $this->nom;}
 	public function getMarque() {return $this->marque;}
+	public function getImage() {return $this->image;}
+	public function getDescription() {return $this->description;}
 	public function getPrix() {return $this->prix;}
 
 	public function setId($id) { $this->id = $id; }
 	public function setNom($nom) { $this->nom = $nom; }
 	public function setMarque($marque) {$this->marque = $marque;}
+	public function setImage($image){$this->image = $image;}
+	public function setDescription($d){$this->description = $d;}
 	public function setPrix($prix) {$this->prix = $prix;}
 }
 ?>
